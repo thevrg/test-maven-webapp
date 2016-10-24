3c9eb6ca404bd68c5da09518821a0c1d0aa59656
Peter Varga <peter.varga@dpc.hu>
2016-10-24 13:57:39 +0200

    @ResponseBody, @PathVariable

--- src/main/java/hu/dpc/edu/web/FirstController.java
+++ src/main/java/hu/dpc/edu/web/FirstController.java
@@ -4,6 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.function.ToIntFunction;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Created by vrg on 24/10/16.
  */
@@ -11,8 +18,8 @@ import org.springframework.web.bind.annotation.*;
 public class FirstController {
 
     @ModelAttribute("message")
-    public String message() {
-        return "Hello World!";
+    public String message(@RequestParam(required = false, defaultValue = "Hello world!") String message) {
+        return message;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -22,8 +29,41 @@ public class FirstController {
     }
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(Model model, @RequestParam(value="name", required = false, defaultValue = "Akárki") String nev) {
+    public String hello(Model model, @RequestParam(value="name", required = false, defaultValue = "Akárki") String nev, HttpServletRequest request) {
         model.addAttribute("szoveg", "Hello, dear " + nev + "!");
+        model.addAttribute("acceptHeader", request.getHeader("Accept"));
         return "udvozlet";
     }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    @ResponseBody
+    public String add(@RequestParam(name="value") String[] values) {
+
+//        new ToIntFunction<String>() {
+//            @Override
+//            public int applyAsInt(String value) {
+//                return Integer.parseInt(value);
+//            }
+//        }
+
+        final int sum = Stream.of(values)
+                .mapToInt(Integer::parseInt)
+                .sum();
+        return "Result is : " + sum;
+    }
+
+    @RequestMapping(value = "/add/{szam1:\\d{2}}/{szam2:\\d{2,5}}", method = RequestMethod.GET)
+    @ResponseBody
+    public String add2(@PathVariable(name="szam1") int szam1, @PathVariable(name="szam2") int szam2) {
+        final int sum = szam1 + szam2;
+        return "Result is : " + sum;
+    }
+
+
+    @RequestMapping(value = "/add3/{szam1:\\d{2}}/{szam2:\\d{2,5}}", method = RequestMethod.GET)
+    public void add3(@PathVariable(name="szam1") int szam1, @PathVariable(name="szam2") int szam2, Writer writer) throws IOException {
+        final int sum = szam1 + szam2;
+        writer.write("Result is : " + sum);
+    }
+
 }
--- src/main/webapp/WEB-INF/jsp/udvozlet.jsp
+++ src/main/webapp/WEB-INF/jsp/udvozlet.jsp
@@ -3,5 +3,6 @@
 <h1>udvozlet</h1>
 <h2>Szoveg: ${szoveg}</h2>
 <h2>Message: ${message}</h2>
+<h3>Accept: ${acceptHeader}</h3>
 </body>
 </html>
